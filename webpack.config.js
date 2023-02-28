b9e24122f16e790325a741db85520216950100ec
Abdus Sattar <sasorganicagro@gmail.com>
2023-02-28 07:16:42 +0600

    Modify webpack.config file for loading html page correctly

--- webpack.config.js
+++ webpack.config.js
@@ -21,7 +21,6 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, './src/index.html'),
-      filename: 'toDoList.html',
     }),
   ],
   module: {
